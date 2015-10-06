39fa63ad74a5274865cff29e80b16880829e2887
changhoonhahn <chh327@nyu.edu>
2015-10-06 15:23:57 -0400

    l2 distance implement for sSFR distribution differences

--- ssfr.py
+++ ssfr.py
@@ -9,6 +9,8 @@ import numpy as np
 
 # -- Local --
 from cenque import CenQue
+from util.cenque_utility import get_q_ssfr_mean
+from sfms.fitting import get_param_sfr_mstar_z
 from group_catalog.group_catalog import central_catalog
 
 class Ssfr(object): 
@@ -112,7 +114,6 @@ class Ssfr(object):
 
         return mass_bins
 
-
 def ssfr_cq_evol(
         start_nsnap = 13, 
         final_nsnap = 1, 
@@ -150,3 +151,93 @@ def ssfr_cq_evol(
     evolved_cq_ssfr = Ssfr()
 
     return evolved_cq_ssfr.cenque(evolved_cq)
+
+
+def rho_ssfr_cq_evol(
+        start_nsnap = 13, 
+        final_nsnap = 1, 
+        sf_prop = {'name': 'average'}, 
+        fq_prop = {'name': 'wetzelsmooth'}, 
+        tau_prop = {'name': 'instant'}, 
+        mass_evol = 'sham', 
+        Mrcut=18, 
+        **kwargs
+        ): 
+    """ Compare sSFR distribution of evolved CenQue and
+    SDSS Group Catalog in the green valley
+    """
+
+    if Mrcut == 18: 
+        z_med = 0.03
+    elif Mrcut == 19: 
+        z_med = 0.05
+    elif Mrcut == 20: 
+        z_med = 0.08
+
+    evol_cq_ssfr_bin, evol_cq_ssfr_hist = ssfr_cq_evol(
+            start_nsnap = start_nsnap, 
+            final_nsnap = final_nsnap, 
+            sf_prop = sf_prop, 
+            fq_prop = fq_prop, 
+            tau_prop = tau_prop, 
+            mass_evol = mass_evol, 
+            **kwargs
+            )
+
+    group_ssfr = Ssfr()
+    group_ssfr_bin, group_ssfr_hist = group_ssfr.groupcat(Mrcut=Mrcut)
+    
+    l2_ssfr = 0.0
+    for i_massbin, massbin in enumerate(group_ssfr.mass_bins): 
+
+        if not np.array_equal(evol_cq_ssfr_bin[i_massbin], group_ssfr_bin[i_massbin]):
+            raise ValueError()
+
+        # sSFR comparison range
+
+        q_ssfr_massbin = np.mean(get_q_ssfr_mean(massbin)) 
+
+        sfr_mstar_z, sig_sfr_mstar_z = get_param_sfr_mstar_z()
+
+        sf_ssfr_massbin = sfr_mstar_z(np.mean(massbin), z_med) - np.mean(massbin)
+
+        ssfr_range = np.where(
+                (evol_cq_ssfr_bin > q_ssfr_massbin) &
+                (evol_cq_ssfr_bin < sf_ssfr_massbin)
+                )
+
+        l2_ssfr += np.sum((evol_cq_ssfr_hist[i_massbin] - group_ssfr_hist[i_massbin])**2)
+
+    print l2_ssfr
+    return l2_ssfr
+
+if __name__ == "__main__":
+    rho_ssfr_cq_evol(
+        start_nsnap = 13, 
+        final_nsnap = 1, 
+        sf_prop = {'name': 'average'}, 
+        fq_prop = {'name': 'wetzelsmooth'}, 
+        tau_prop = {'name': 'instant'}, 
+        mass_evol = 'sham', 
+        Mrcut=18
+        ) 
+
+    rho_ssfr_cq_evol(
+        start_nsnap = 13, 
+        final_nsnap = 1, 
+        sf_prop = {'name': 'average'}, 
+        fq_prop = {'name': 'wetzelsmooth'}, 
+        tau_prop = {'name': 'satellite'}, 
+        mass_evol = 'sham', 
+        Mrcut=18
+        ) 
+
+    rho_ssfr_cq_evol(
+        start_nsnap = 13, 
+        final_nsnap = 1, 
+        sf_prop = {'name': 'average'}, 
+        fq_prop = {'name': 'wetzelsmooth'}, 
+        tau_prop = {'name': 'line', 'fid_mass': 10.75, 'slope': -0.57, 'yint': 0.5}, 
+        mass_evol = 'sham', 
+        Mrcut=18
+        ) 
