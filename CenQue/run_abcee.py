e4564f4e4db1b5c1e0f3d8a9c5c2b28f44069ed4
changhoonhahn <chh327@nyu.edu>
2016-04-18 00:44:52 +0000

    edits in run_abcee in order to run fqz_multi option

--- CenQue/run_abcee.py
+++ CenQue/run_abcee.py
@@ -11,13 +11,29 @@ if restart == 0:
     print 'N_iterations = ', Niter
     Npart = int(sys.argv[3])
     print 'N_particle = ', Npart
-    abcrun = sys.argv[4]
+    obvs = int(sys.argv[4])
+    if obvs == 0: 
+        print 'ssfr ONLY'
+        obvs_list = ['ssfr'] 
+        guess = [10.] 
+    elif obvs == 1: 
+        print 'ssfr + f_Q(z=0.34)'
+        obvs_list = ['ssfr', 'fqz03'] 
+    elif obvs == 2: 
+        print 'ssfr + [f_Q(z=0.05), f_Q(z=0.16), f_Q(z=0.3)]'
+        obvs_list = ['ssfr', 'fqz_multi'] 
+    else: 
+        raise ValueError 
+    abcrun = sys.argv[5]
     print 'ABC run name = ', abcrun
 
-    ABC(Niter, [10., 10.], Npart=Npart, prior_name='try0', observables=['ssfr', 'fqz_multi'], abcrun=abcrun)
-    #ABC(Niter, [10.], Npart=Npart, prior_name='try0', observables=['ssfr'], abcrun=abcrun)
+    guess = [10. for i in range(len(obvs_list))]
+
+    ABC(Niter, guess, Npart=Npart, prior_name='try0', observables=obvs_list, abcrun=abcrun)
 
 elif restart == 1:  
+    raise NotImplementedError('Reimplement... carefully.')
+
     Niter = int(sys.argv[2])
     print 'N_iterations = ', Niter
     Npart = int(sys.argv[3])
@@ -27,8 +43,6 @@ elif restart == 1:
     trestart = int(sys.argv[5])
     print 'Restarting abc from t = ', str(trestart)
 
-    ABC(Niter, [4.22460181, 0.33794247], Npart=Npart, prior_name='try0', observables=['ssfr', 'fqz03'], 
-            abcrun=abcrun, t_restart=trestart, restart=True)
-
-    #ABC(Niter, [4.28186054117], Npart=Npart, prior_name='try0', observables=['ssfr'], 
-    #        abcrun=abcrun, t_restart=trestart, restart=True)
+    ABC(Niter, [4.22460181, 0.33794247], Npart=Npart, prior_name='try0', 
+            observables=['ssfr', 'fqz03'], abcrun=abcrun, t_restart=trestart, 
+            restart=True)
