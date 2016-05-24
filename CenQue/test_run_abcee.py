165c8fc02472ff3950b44bdad5158f9f981b1ac0
Chang Hoon Hahn <chh327@login-0-3.local>
2016-05-24 14:51:34 -0400

    test_run_abcee.py for no smf evol

--- CenQue/test_run_abcee.py
+++ CenQue/test_run_abcee.py
@@ -7,5 +7,5 @@ from abcee import ABC
 # Test the SMF evolution's impact on the quenching parameter constraints 
 obvs_list = ['ssfr', 'fqz_multi'] 
 guess = [10. for i in range(len(obvs_list))]
-ABC(3, guess, Npart=10, prior_name='updated', observables=obvs_list, 
+ABC(20, guess, Npart=1000, prior_name='updated', observables=obvs_list, 
         abcrun='multifq_wideprior_nosmfevo', subhalo='constant-li')
